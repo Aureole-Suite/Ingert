155b148025e1d899264b4a54942406cb2ebc6ae1
Kyuuhachi <caagr98@gmail.com>
2024-10-29 21:05:35 +0100

    Rewrite printer to produce tokens
    Had to sacrifice alignment, but I'll be readding that

--- src/print.rs
+++ src/print.rs
@@ -16,48 +16,59 @@ impl Default for Settings {
 	}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum Space {
+	Tight,
+	Line(u32),
+	Space,
+}
+
+#[derive(Debug, Clone)]
+struct Token {
+	line: Option<u16>,
+	text: String,
+	space: Space,
+}
+
 struct Ctx {
 	settings: Settings,
 	indent: u32,
-	out: String,
-	line: u16,
+	out: Vec<Token>,
+	next_line: Option<u16>,
 }
 
 impl Ctx {
-	fn write_fmt(&mut self, args: std::fmt::Arguments) {
-		use std::fmt::Write;
-		self.out.write_fmt(args).unwrap()
+	fn tight(&mut self) -> &mut Self {
+		if let Some(token) = self.out.last_mut() {
+			token.space = token.space.min(Space::Tight);
+		}
+		self
 	}
 
-	fn space(&mut self, line: Option<u16>, inline: bool) {
-		let lines = if self.settings.use_lines {
-			line.unwrap_or(self.line).saturating_sub(self.line).min(10)
-		} else {
-			!(inline as u16)
-		};
-		if lines == 0 {
-			if !inline {
-				write!(self, " ");
-			}
-		} else {
-			for _ in 0..lines {
-				writeln!(self)
-			}
-			for _ in 0..self.indent + (!inline as u32) {
-				write!(self, "\t");
-			}
+	fn line(&mut self) -> &mut Self {
+		if let Some(token) = self.out.last_mut() {
+			token.space = token.space.min(Space::Line(self.indent));
 		}
-
-		if let Some(line) = line {
-			self.line = line
-		}
+		self
 	}
 
-	fn space_explicit(&mut self, line: Option<u16>, inline: bool) {
-		self.space(line, inline);
-		if let Some(line) = line && self.settings.show_lines {
-			write!(self, "{line}@");
-		}
+	fn align(&mut self, line: Option<u16>) -> &mut Self {
+		assert_eq!(self.next_line, None);
+		self.next_line = line;
+		self
+	}
+
+	fn semi(&mut self) -> &mut Self {
+		self.tight().token(";")
+	}
+
+	fn token(&mut self, text: impl ToString) -> &mut Self {
+		self.out.push(Token {
+			line: self.next_line.take(),
+			text: text.to_string(),
+			space: Space::Space,
+		});
+		self
 	}
 }
 
@@ -65,36 +76,57 @@ pub fn print(scena: &[Item], settings: Settings) -> String {
 	let mut ctx = Ctx {
 		settings,
 		indent: 0,
-		out: String::new(),
-		line: 0,
+		next_line: None,
+		out: Vec::new(),
 	};
 	for i in scena {
 		item(&mut ctx, i);
 	}
-	ctx.out
+
+	let mut out = String::new();
+	for tok in &ctx.out {
+		use std::fmt::Write;
+		if let Some(l) = tok.line && ctx.settings.show_lines {
+			write!(out, "{l}@").unwrap();
+		}
+		out.push_str(&tok.text);
+		match tok.space {
+			Space::Tight => {},
+			Space::Line(l) => {
+				out.push('\n');
+				for _ in 0..l {
+					out.push('\t');
+				}
+			}
+			Space::Space => out.push(' '),
+		}
+	}
+	out
 }
 
 fn item(ctx: &mut Ctx, item: &Item) {
 	match item {
 		Item::Global(g) => {
-			ctx.space_explicit(g.line, false);
-			write!(ctx, "global {}", g.name);
-			ty(ctx, &g.name, g.ty, false);
-			write!(ctx, ";");
+			ctx.line().align(g.line).token("global");
+			ctx.token(&g.name).tight().token(":");
+			ty(ctx, g.ty);
+			ctx.semi();
 		}
 		Item::Function(f) => {
-			let line = f.body.first_line();
-			ctx.space(line, false);
+			ctx.line();
 			if f.is_prelude {
-				write!(ctx, "prelude ");
+				ctx.token("prelude");
 			}
-			write!(ctx, "function {}", f.name);
+			ctx.token("function").token(&f.name);
 			let mut n = 0;
 			args(ctx, &f.args, |ctx, arg| {
-				let name = format!("arg{}", n);
-				ty(ctx, &name, arg.ty, arg.out);
+				ctx.token(format_args!("arg{}", n)).tight().token(":");
+				if arg.out {
+					ctx.token("&").tight();
+				}
+				ty(ctx, arg.ty);
 				if let Some(def) = &arg.default {
-					write!(ctx, " = ");
+					ctx.token("=");
 					value(ctx, def);
 				}
 				n += 1;
@@ -104,82 +136,80 @@ fn item(ctx: &mut Ctx, item: &Item) {
 	}
 }
 
-fn ty(ctx: &mut Ctx, name: &str, ty: Type, is_ref: bool) {
-	write!(ctx, "{name}: ");
-	if is_ref {
-		write!(ctx, "&");
-	}
+fn ty(ctx: &mut Ctx, ty: Type) {
 	match ty {
-		Type::Number => write!(ctx, "num"),
-		Type::String => write!(ctx, "str"),
-	}
+		Type::Number => ctx.token("num"),
+		Type::String => ctx.token("str"),
+	};
 }
 
 fn args<T>(ctx: &mut Ctx, args: &[T], mut each: impl FnMut(&mut Ctx, &T)) {
-	write!(ctx, "(");
+	ctx.tight().token("(").tight();
 	let mut iter = args.iter();
 	if let Some(arg) = iter.next() {
 		each(ctx, arg);
 		for arg in iter {
-			write!(ctx, ", ");
+			ctx.tight().token(",");
 			each(ctx, arg);
 		}
 	}
-	write!(ctx, ")");
+	ctx.tight().token(")");
 }
 
 fn value(ctx: &mut Ctx, value: &Value) {
 	match value {
-		Value::Int(v) => write!(ctx, "{}", v),
-		Value::Float(v) => write!(ctx, "{:?}", v),
-		Value::String(v) => write!(ctx, "{:?}", v),
-	}
+		Value::Int(v) => ctx.token(format_args!("{v:?}")),
+		Value::Float(v) => ctx.token(format_args!("{v:?}")),
+		Value::String(v) => ctx.token(format_args!("{v:?}")),
+	};
 }
 
 fn block(ctx: &mut Ctx, body: &[Stmt]) {
-	write!(ctx, " {{");
+	ctx.token("{");
 	ctx.indent += 1;
 	for s in body {
+		ctx.line();
 		stmt(ctx, s);
 	}
 	ctx.indent -= 1;
-	ctx.space(None, false);
-	write!(ctx, "}}");
+	// TODO should this line be included if empty?
+	ctx.line();
+	ctx.token("}");
 }
 
 fn stmt(ctx: &mut Ctx, s: &Stmt) {
 	match s {
 		Stmt::Expr(e) => {
 			expr(ctx, e);
-			write!(ctx, ";");
+			ctx.semi();
 		},
 		Stmt::PushVar(lv, e) => {
-			write!(ctx, "let var{}", lv.0);
+			ctx.token("var").token(format_args!("var{}", lv.0));
 			if let Some(e) = e {
-				write!(ctx, " = ");
+				ctx.token("=");
 				expr(ctx, e);
 			}
-			write!(ctx, ";");
+			ctx.semi();
 		}
 		Stmt::Set(lv, e) => {
 			lvalue(ctx, lv);
-			write!(ctx, " = ");
+			ctx.token("=");
 			expr(ctx, e);
-			write!(ctx, ";");
+			ctx.semi();
 		}
 		Stmt::Line(l) => {
-			ctx.space_explicit(Some(*l), false);
+			ctx.align(Some(*l));
 		}
 		Stmt::Debug(es) => {
-			write!(ctx, "debug");
+			ctx.token("debug");
 			args(ctx, es, expr);
 		}
 		Stmt::If(a, b, c) => {
-			write!(ctx, "if ");
+			ctx.token("if");
 			expr(ctx, a);
 			block(ctx, b);
 			if let Some(c) = c {
-				write!(ctx, " else ");
+				ctx.token("else");
 				if matches!(c.as_slice(), [Stmt::If(..)] | [Stmt::Line(..), Stmt::If(..)]) {
 					for s in c {
 						stmt(ctx, s);
@@ -190,43 +220,42 @@ fn stmt(ctx: &mut Ctx, s: &Stmt) {
 			}
 		}
 		Stmt::While(a, b) => {
-			write!(ctx, "while ");
+			ctx.token("while");
 			expr(ctx, a);
 			block(ctx, b);
 		}
 		Stmt::Switch(a, b) => {
-			write!(ctx, "switch ");
+			ctx.token("switch");
 			expr(ctx, a);
-			write!(ctx, " {{");
+			ctx.token("{");
 			for (n, s) in b {
-				ctx.space(None, false);
 				if let Some(n) = n {
-					write!(ctx, "case {n}:");
+					ctx.line().token("case").token(n).tight().token(":");
 				} else {
-					write!(ctx, "default:");
+					ctx.line().token("default").tight().token(":");
 				}
 				ctx.indent += 1;
 				for s in s {
+					ctx.line();
 					stmt(ctx, s);
 				}
 				ctx.indent -= 1;
 			}
-			ctx.space(None, false);
-			write!(ctx, "}}");
+			ctx.line();
+			ctx.token("}");
 		}
 		Stmt::Break => {
-			write!(ctx, "break;");
+			ctx.token("break").semi();
 		}
 		Stmt::Continue => {
-			write!(ctx, "continue;");
+			ctx.token("continue").semi();
 		}
 		Stmt::Return(v) => {
-			write!(ctx, "return");
+			ctx.token("return");
 			if let Some(v) = v {
-				write!(ctx, " ");
 				expr(ctx, v);
 			}
-			write!(ctx, ";");
+			ctx.semi();
 		}
 	}
 }
@@ -235,10 +264,12 @@ fn lvalue(ctx: &mut Ctx, lv: &Lvalue) {
 	match lv {
 		Lvalue::Stack(s) => var(ctx, *s),
 		Lvalue::Deref(s) => {
-			write!(ctx, "*");
+			ctx.token("*").tight();
 			var(ctx, *s);
 		}
-		Lvalue::Global(n) => write!(ctx, "{}", n),
+		Lvalue::Global(n) => {
+			ctx.token(n);
+		}
 	}
 }
 
@@ -251,41 +282,36 @@ fn expr(ctx: &mut Ctx, e: &Expr) {
 fn expr0(ctx: &mut Ctx, e: &Expr, prio: u32) {
 	match e {
 		Expr::Value(l, v) => {
-			ctx.space_explicit(*l, true);
+			ctx.align(*l);
 			value(ctx, v)
 		}
 		Expr::Var(l, v) => {
-			ctx.space_explicit(*l, true);
+			ctx.align(*l);
 			lvalue(ctx, v)
 		}
 		Expr::Ref(l, v) => {
-			ctx.space_explicit(*l, true);
-			write!(ctx, "&");
+			ctx.align(*l).token("&").tight();
 			var(ctx, *v);
 		}
 		Expr::Call(l, c, a) => {
-			ctx.space_explicit(*l, true);
+			ctx.align(*l);
 			call(ctx, c);
 			args(ctx, a, |ctx, e| expr0(ctx, e, 0));
 		}
 		Expr::Unop(l, o, a) => {
-			ctx.space_explicit(*l, true);
-			write!(ctx, "{}", o);
+			ctx.align(*l).token(o).tight();
 			expr0(ctx, a, 10);
 		}
 		Expr::Binop(l, o, a, b) => {
 			let p = op_prio(*o);
 			if p < prio {
-				write!(ctx, "(");
+				ctx.token("(").tight();
 			}
 			expr0(ctx, a, p);
-			write!(ctx, " ");
-			ctx.space_explicit(*l, true);
-			write!(ctx, "{}", o);
-			write!(ctx, " ");
+			ctx.align(*l).token(o);
 			expr0(ctx, b, p + 1);
 			if p < prio {
-				write!(ctx, ")");
+				ctx.tight().token(")");
 			}
 		}
 	}
@@ -293,81 +319,16 @@ fn expr0(ctx: &mut Ctx, e: &Expr, prio: u32) {
 
 fn call(ctx: &mut Ctx, c: &CallKind) {
 	match c {
-		CallKind::System(a, b) => write!(ctx, "system[{a},{b}]"),
-		CallKind::Func(a) => write!(ctx, "{a}"),
-		CallKind::Tail(a) => write!(ctx, "tailcall {a}"),
-	}
+		CallKind::System(a, b) => ctx.token(format_args!("system[{a},{b}]")),
+		CallKind::Func(a) => ctx.token(a),
+		CallKind::Tail(a) => ctx.token("tailcall").token(a),
+	};
 }
 
 fn var(ctx: &mut Ctx, v: StackVar) {
 	if v.0 < 0 {
-		write!(ctx, "arg{}", -v.0);
+		ctx.token(format_args!("arg{}", -v.0));
 	} else {
-		write!(ctx, "var{}", v.0);
-	}
-}
-
-trait FirstLine {
-	fn first_line(&self) -> Option<u16>;
-}
-
-impl<T: FirstLine> FirstLine for [T] {
-	fn first_line(&self) -> Option<u16> {
-		self.iter().find_map(T::first_line)
-	}
-}
-
-impl<T: FirstLine> FirstLine for Vec<T> {
-	fn first_line(&self) -> Option<u16> {
-		self.as_slice().first_line()
-	}
-}
-
-impl<T: FirstLine> FirstLine for Option<T> {
-	fn first_line(&self) -> Option<u16> {
-		self.as_ref().and_then(T::first_line)
-	}
-}
-
-impl<T: FirstLine + ?Sized> FirstLine for Box<T> {
-	fn first_line(&self) -> Option<u16> {
-		T::first_line(self)
-	}
-}
-
-impl<T: FirstLine + ?Sized> FirstLine for &mut T {
-	fn first_line(&self) -> Option<u16> {
-		T::first_line(self)
-	}
-}
-
-impl FirstLine for Stmt {
-	fn first_line(&self) -> Option<u16> {
-		match self {
-			Stmt::Expr(e) => e.first_line(),
-			Stmt::PushVar(_, e) => e.first_line(),
-			Stmt::Set(_, e) => e.first_line(),
-			Stmt::Line(l) => Some(*l),
-			Stmt::Debug(es) => es.first_line(),
-			Stmt::If(a, b, c) => a.first_line().or_else(|| b.first_line()).or_else(|| c.first_line()),
-			Stmt::While(a, b) => a.first_line().or_else(|| b.first_line()),
-			Stmt::Switch(a, b) => a.first_line().or_else(|| b.iter().find_map(|(_, s)| s.first_line())),
-			Stmt::Break => todo!(),
-			Stmt::Continue => todo!(),
-			Stmt::Return(v) => v.first_line(),
-		}
-	}
-}
-
-impl FirstLine for Expr {
-	fn first_line(&self) -> Option<u16> {
-		match self {
-			Expr::Value(l, _) => *l,
-			Expr::Var(l, _) => *l,
-			Expr::Ref(l, _) => *l,
-			Expr::Call(l, _, args) => l.or_else(|| args.first_line()),
-			Expr::Unop(l, _, a) => l.or_else(|| a.first_line()),
-			Expr::Binop(l, _, a, b) => a.first_line().or(*l).or_else(|| b.first_line()),
-		}
+		ctx.token(format_args!("var{}", v.0));
 	}
 }
