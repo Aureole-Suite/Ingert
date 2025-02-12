e117234b4cc347f6a5b686a9c392c197c2ff679f
Kyuuhachi <caagr98@gmail.com>
2025-02-12 18:48:43 +0100

    Store tree in body

--- src/scena.rs
+++ src/scena.rs
@@ -2,13 +2,13 @@ mod flat;
 
 use crate::scp::{Op, Scp};
 pub use crate::scp::{Arg, Binop, CallKind, GlobalType, Unop, Value};
+pub use flat::FlatStmt;
 
 pub fn decompile(scp: &Scp) -> Scena {
 	let mut globals = scp.globals.iter().rev();
 	let mut items = Vec::new();
 	for f in scp.functions.iter().rev() {
 		let _span = tracing::info_span!("decompile", name = f.name).entered();
-		dbg!(f);
 		let mut code = f.code.as_slice();
 		while let Some(Op::Line(n)) = code.last() && let Some(g) = globals.next() {
 			code = &code[..code.len() - 1];
@@ -18,10 +18,17 @@ pub fn decompile(scp: &Scp) -> Scena {
 				line: Some(*n),
 			}));
 		}
-		tracing::info!("{:#?}", flat::decompile(code));
+		let body = match flat::decompile(code) {
+			Ok(body) => Body::Flat(body),
+			Err(e) => {
+				tracing::error!("decompile error: {e}");
+				Body::Asm(code.to_vec())
+			},
+		};
 		items.push(Item::Function(Function {
 			name: f.name.clone(),
 			args: f.args.clone(),
+			body,
 		}));
 	}
 	for g in globals {
@@ -57,7 +64,14 @@ pub struct Global {
 pub struct Function {
 	pub name: String,
 	pub args: Vec<Arg>,
-	// ...
+	pub body: Body,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Body {
+	Asm(Vec<Op>),
+	Flat(Vec<FlatStmt>),
+	Tree(()),
 }
 
 #[derive(Debug, Clone, PartialEq)]
