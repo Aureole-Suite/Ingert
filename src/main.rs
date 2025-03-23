188c91f17a823f354ce058866b22bc9b202c0666
Kyuuhachi <caagr98@gmail.com>
2025-03-23 20:40:03 +0100

    Delete legacy and unused deps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(let_chains)]
 use clap::Parser;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use tracing_subscriber::prelude::*;
 
 #[derive(clap::Parser)]
@@ -16,7 +16,6 @@ fn main() {
 	unsafe { compact_debug::enable(true) };
 	let args = Args::parse();
 
-	let mut prelude = ingert::legacy::prelude::Prelude::new();
 	for file in &args.files {
 		let _span = tracing::info_span!("file", path = %file.display()).entered();
 		if file.ends_with("mon9996_c00.da") {
@@ -33,7 +32,7 @@ fn main() {
 		// let scp = ingert::legacy::scp::parse_scp(&data).unwrap();
 		let scp2 = ingert::scp::read(&data).unwrap();
 		let mut scena2 = ingert::scena::from_scp(scp2.clone());
-		let data2 = ingert::scp::write(&scp2).unwrap();
+		let _data2 = ingert::scp::write(&scp2).unwrap();
 		ingert::scena::decompile(&mut scena2);
 		// println!("{:#?}", scena);
 		// std::fs::write("a.bin", &data).unwrap();
@@ -43,19 +42,4 @@ fn main() {
 
 		// print(file.file_name().unwrap(), &scena);
 	}
-
-	print("prelude.da", &prelude.into_scena());
-}
-
-fn print(name: impl AsRef<Path>, scena: &[ingert::legacy::Item]) {
-	let out = Path::new("out").join(name);
-	let _span = tracing::info_span!("print", path = %out.display()).entered();
-	let printed = ingert::legacy::print::print(scena, ingert::legacy::print::Settings {
-		show_lines: true,
-		..ingert::legacy::print::Settings::default()
-	});
-	std::fs::write(out, &printed).unwrap();
-
-	let (a, b) = ingert::legacy::parse::parse(&printed).unwrap();
-	similar_asserts::assert_eq!(scena, &a);
 }
