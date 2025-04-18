e3a43581e649d7f0ea87080d7a92499ac0d52763
Kyuuhachi <caagr98@gmail.com>
2025-04-18 09:09:01 +0200

    Function for making an iterator of args

--- ingert-syntax/src/lib.rs
+++ ingert-syntax/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(let_chains, if_let_guard, never_type)]
+#![feature(let_chains, if_let_guard, never_type, iter_order_by)]
 
 use ingert::scena::{Expr, Place, Stmt, Var};
 
@@ -15,30 +15,25 @@ struct SyscallWrapper {
 }
 
 impl SyscallWrapper {
-	fn from_tree(body: &[Stmt], nargs: usize) -> Option<SyscallWrapper> {
+	fn args(nargs: usize) -> impl Iterator<Item = Expr> {
+		(0..nargs)
+			.rev()
+			.map(|i| Expr::Var(None, Place::Var(Var(i as u32))))
+	}
+
+	pub fn from_tree(body: &[Stmt], nargs: usize) -> Option<SyscallWrapper> {
 		let (ret, a, b, args) = match body {
 			[Stmt::Return(None, Some(Expr::Syscall(None, a, b, args)))] => (true, *a, *b, args),
 			[Stmt::Expr(Expr::Syscall(None, a, b, args)), Stmt::Return(None, None)] => (false, *a, *b, args),
 			_ => return None,
 		};
-
-		if args.len() != nargs {
-			return None;
-		}
-
-		for (i, arg) in args.iter().rev().enumerate().rev() {
-			if arg != &Expr::Var(None, Place::Var(Var(i as u32))) {
-				return None;
-			}
-		}
-		Some(SyscallWrapper { ret, a, b })
+		Self::args(nargs)
+			.eq_by(args, |a, b| a == *b)
+			.then_some(SyscallWrapper { ret, a, b })
 	}
 
-	fn as_tree(&self, nargs: usize) -> Vec<Stmt> {
-		let args = (0..nargs)
-			.rev()
-			.map(|i| Expr::Var(None, Place::Var(Var(i as u32))))
-			.collect();
+	pub fn as_tree(&self, nargs: usize) -> Vec<Stmt> {
+		let args = Self::args(nargs).collect();
 		let syscall = Expr::Syscall(None, self.a, self.b, args);
 		if self.ret {
 			vec![Stmt::Return(None, Some(syscall))]
